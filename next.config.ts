e1c43e6f40ec8aaac63a498538d900137c466a0d
SdneyFernandes <fsidney987@gmail.com>
2025-08-25 00:21:47 -0300

    i

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  trailingSlash: true,
+  trailingSlash: true, 
   images: {
-    unoptimized: true
+    unoptimized: true, 
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  // ❌ REMOVA as rewrites - não são mais necessárias
+  
 };
 
 export default nextConfig;
